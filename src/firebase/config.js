4377c22dbba1ea1e3a826bfb6076d1430d426144
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-06-01 15:18:33 -0700

    Add create functionality

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -17,5 +17,6 @@ firebase.initializeApp(firebaseConfig);
 // Initilize Service
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth };
+export { projectFirestore, projectAuth, timestamp };
---
+++ src/pages/home/TransactionForm.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import { useFirestore } from "../../hooks/useFirestore";
+
+function TransactionForm({ uid }) {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
+  const { addDocument, response } = useFirestore("transactions");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addDocument({ uid, name, amount });
+  };
+
+  // Reset Form fields
+  useEffect(() => {
+    if (response.success) {
+      setName("");
+      setAmount("");
+    }
+  }, [response.success]);
+
+  return (
+    <>
+      <h3>Add Transaciton</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Transaction Name:</span>
+          <input
+            type="text"
+            required
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          ></input>
+        </label>
+        <label>
+          <span>Amount ($):</span>
+          <input
+            type="number"
+            required
+            onChange={(e) => setAmount(e.target.value)}
+            value={amount}
+          ></input>
+        </label>
+        <button>Add Transaction</button>
+      </form>
+    </>
+  );
+}
+
+export default TransactionForm;
